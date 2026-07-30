78ce33e22858eef1136a95968bc1009d9b698c37
Shiksha Pandey <shikshapandey2004@gmail.com>
2026-07-30 15:01:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.c
@@ -0,0 +1,10 @@
+int largest(int arr[], int n) {
+    // Code Here
+    int largest=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+       
+    } return largest;
+}
